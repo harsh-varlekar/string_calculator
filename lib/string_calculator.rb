4dad758ca0431c4ba9358b5bff191113ae4551ef
Harsh Varlekar <harsh29varlekar@gmail.com>
2025-04-16 12:51:00 +0530

    Refactor add method for readability and maintainability

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,19 +1,19 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.strip.empty?
-  
+
     delimiter = ","
     if numbers.start_with?("//")
       delimiter_line, numbers = numbers.split("\n", 2)
       delimiter = delimiter_line[2]
     end
-  
-    numbers = numbers.gsub("\n", delimiter)
-    numbers.split(delimiter).map(&:to_i).sum
 
+    numbers = numbers.gsub("\n", delimiter)
     nums = numbers.split(delimiter).map(&:to_i)
+
     negatives = nums.select { |n| n < 0 }
     raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+
     nums.sum
-  end  
+  end
 end
